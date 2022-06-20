e5977180e87070c8ce5d1f9b931bae5a91f4af69
Maddy <madisonschaaf48@gmail.com>
2022-06-19 21:11:58 -0600

    redo repo adding server and client files

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,53 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_EXERCISE = gql`
+  mutation addExercise($exerciseName: String!) {
+    addExercise(exerciseName: $exerciseName) {
+      _id
+      exerciseName
+      userExercise {
+       _id
+      }
+    }
+  }
+`;
+
+export const ADD_USER_EXERCISE = gql`
+  mutation addUserExercise($exerciseId: ID!, $weight: Int, $repetitions: Int, $time: Int, $notes: String) {
+    addUserExercise(exerciseId: $exerciseId, weight: $weight, repetitions: $repetitions, time: $time, notes: $notes ) {
+      _id
+      userExercise {
+        _id
+        weight
+      repetitions
+      username
+      notes
+      createdAt
+      }
+    }
+  }
+`;
