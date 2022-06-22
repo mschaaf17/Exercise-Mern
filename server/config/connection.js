e283e882bad0229d36ba88b5131505992a2d1faf
Neo <fullstackneo@gmail.com>
2022-06-21 22:46:26 -0600

    modify databse name

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 // mongoose port name change?
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/practice', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/stacked', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
