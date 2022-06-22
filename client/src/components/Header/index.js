7080715f74007e2df1ca39b6c1f3d469baa573cb
Carl Wheezer <slushern42@gmail.com>
2022-06-22 00:17:49 -0600

    added video background to homepage

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -13,7 +13,7 @@ const Header = () => {
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link to="/">
-          <h1>My Fitness Plan</h1>
+          <h1>Stacked</h1>
         </Link>
 
         <nav className="text-center">
@@ -37,3 +37,4 @@ const Header = () => {
 };
 
 export default Header;
+
