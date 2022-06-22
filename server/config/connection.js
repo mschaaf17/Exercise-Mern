9712fdcb04a03a569aa51ceaf151a1b2b157f071
Neo <fullstackneo@gmail.com>
2022-06-21 22:16:49 -0600

    project set-up configuration

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 // mongoose port name change?
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/practice', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/practice', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
