62d85bacb93542f9e493f6c24328c1b96b86dafd
Maddy <madisonschaaf48@gmail.com>
2022-06-21 23:17:39 -0600

    added backend userInfo to workout

--- client/src/pages/Workout.js/index.js
+++ client/src/pages/Workout.js/index.js
@@ -24,6 +24,67 @@ export default function Workout() {
 
     }, [timerOn])
 
+    const [exerciseState, setExerciseState] = useState({
+        exerciseName: ''
+    })
+    const [addExercise] = useMutation(ADD_EXERCISE)
+
+    const [exerciseInfoState, setExerciseInfoState] = useState({
+      weight: '',
+      repetitions: '',
+      notes: '',
+    })
+    const [addUserExercise] = useMutation(ADD_USER_EXERCISE)
+
+
+    // update state based on input changes
+    const handleChange = (event) => {
+        const {name, value} = event.target
+        setExerciseState({
+            ...exerciseState,
+            [name]: value
+        })
+        setExerciseInfoState({
+            ...exerciseInfoState,
+            [name]: value
+        })
+    }
+
+    // submit form
+    const handleFormSubmit = async (event) => {
+        event.preventDefault()
+
+        try {
+            const {data} = await addExercise({
+                variables: {...exerciseState}
+            })
+            const {moreData } = await addUserExercise({
+                variables: {...exerciseInfoState}
+            })
+            console.log({data})
+            console.log({moreData})
+        }
+        catch (e) {
+            console.error (e)
+        }
+        // clear all values
+        setExerciseState({
+            exerciseName: ''
+        })
+        setExerciseInfoState({
+            weight: '',
+            repetitions: '',
+            notes: ''
+        })
+        console.log('Submitted!')
+    }
+
+
+    // logged exercises form 
+    // if(!data.length) {
+    //     return <h3>No exercises logged yet.</h3>
+    // }
+
   return (
     <div>
         This is the workout page!
@@ -58,24 +119,27 @@ export default function Workout() {
         {/* enter exercise area */}
     <div className="border">
       <h1>Log your Workout</h1>
-      <form>
-        <input placeholder="Exercise Name" name="exerciseName" type="text" value= 'exerciseName'/>
-        <input placeholder="weight" name="weight" type="text" value= 'weight'/>
-        <input placeholder="repetitions" name="repetitions" type="text" value= 'repetitions'/>
+      <form onSubmit = {handleFormSubmit}>
+        <input placeholder="Exercise Name" name="exerciseName" type="text" value= {exerciseState.exerciseName} onChange = {handleChange}/>
+        <input placeholder="weight" name="weight" type="text" value= {exerciseInfoState.weight} onChange = {handleChange}/>
+        <input placeholder="repetitions" name="repetitions" type="text" value= {exerciseInfoState.repetitions} onChange = {handleChange}/>
         <p placeholder="cardioTime" name="cardioTime" type="text" value= {time}>   <span>{("0" + Math.floor((time / 3600000) % 60)).slice(-2)}:</span>
         <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
         <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}</span></p>
-        <textarea placeholder="notes" name="notes" type="text" value= 'notes'/>
+        <textarea placeholder="notes" name="notes" type="text" value= {exerciseInfoState.notes} onChange = {handleChange}/>
         <button type="submit">Submit</button>
       </form>
     </div>
+
+    <div>
     <h1>Workout Log</h1>
     <div>
-        6/21/22
+    date the workout was logged
     </div>
     <div>
-        <span>30:01</span>
-        <span>running, push ups</span>
+        <span>{time}</span>
+        <span>createdAt</span>
+    </div>
     </div>
     </div>
   )
