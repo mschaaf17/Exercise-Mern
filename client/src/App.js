3d8b10c64bcd58251ed843df082631bf598cad98
Carl Wheezer <slushern42@gmail.com>
2022-06-21 21:15:23 -0600

    Login and Signup routes added to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,8 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -39,13 +41,24 @@ function App() {
         <div className="flex-column justify-flex-start min-100-vh">
           <Header />
           <div className="container">
-            hey
-            {/* <Routes>
+            <Routes>
               <Route 
                 path="/" 
                 element={<Home />} 
               />
-            </Routes> */}
+              <Route 
+                path="/login"
+                element={<Login />}
+              />
+              <Route
+                path="/signup"
+                element={<Signup />}
+              />
+              {/* <Route 
+                path="*"
+                element={<NoMatch />}
+              /> */}
+            </Routes>
           </div>
           <Footer />
         </div>
