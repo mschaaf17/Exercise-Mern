235979248690ea082f7f233ccc1ca91ac6d56a38
Neo <fullstackneo@gmail.com>
2022-06-23 01:04:11 -0600

    add css to make all charts align to left

--- client/src/pages/Ranking/index.js
+++ client/src/pages/Ranking/index.js
@@ -18,34 +18,29 @@ function Ranking() {
 
   return (
     <div className="mainbox">
-      <div className="column">
-        <div className="panel bar">
-          <h2>Exercise Hours</h2>
-          <div className="chart hour"></div>
-          <div className="panel-footer"></div>
-        </div>
+      <div className="panel">
+        <h2>Exercise Hours</h2>
+        <div className="chart hour"></div>
+        <div className="panel-footer"></div>
       </div>
-      <div className="column">
-        <div className="panel line">
-          <h2>Weight Change</h2>
-          <div className="chart weight"></div>
-          <div className="panel-footer"></div>
-        </div>
+      <div className="panel">
+        <h2>Top 5 Players</h2>
+        <div className="chart ranking"></div>
+        <div className="panel-footer"></div>
       </div>
-      <div className="column">
-        <div className="panel line">
-          <h2>Top5 Players of Last Week</h2>
-          <div className="chart ranking"></div>
-          <div className="panel-footer"></div>
-        </div>
+      <div className="panel">
+        <h2>Weight Track</h2>
+        <div className="chart weight"></div>
+        <div className="panel-footer"></div>
       </div>
-      <div className="column">
-        <div className="panel line">
-          <h2>Exercise Analysis</h2>
-          <div className="chart analysis"></div>
-          <div className="panel-footer"></div>
-        </div>
+
+      <div className="panel">
+        <h2>Exercise Analysis</h2>
+        <div className="chart analysis"></div>
+        <div className="panel-footer"></div>
       </div>
+      <div className="panel panel-blank"></div>
+      <div className="panel panel-blank"></div>
     </div>
   );
 }
