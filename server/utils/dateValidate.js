1dda953f332fddb2d833078f04db263fdc7b7a3a
Neo <fullstackneo@gmail.com>
2022-06-24 00:42:09 -0600

    validate a date within last week

---
+++ server/utils/dateValidate.js
@@ -0,0 +1,22 @@
+const withInLastWeek = utcTime => {
+  let Day = new Date(utcTime);
+  const dateArray = Day.toLocaleString().split(',')[0].split('/');
+  Day = dateArray[2] + '/' + dateArray[0] + '/' + dateArray[1];
+
+  let date = new Date();
+
+  date.setDate(date.getDate() - 7 - date.getDay() + 1);
+
+  const lastMonday =
+    date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + date.getDate();
+  // console.log(lastMonday);
+
+  date.setDate(date.getDate() + 6);
+
+  const lastSunday =
+    date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + date.getDate();
+  console.log(lastMonday, lastSunday, Day);
+  return lastMonday <= Day && lastSunday >= Day;
+};
+
+module.exports = { withInLastWeek };
