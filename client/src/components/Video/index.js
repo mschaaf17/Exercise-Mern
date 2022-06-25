5bcc3e5a226dbc0ff0b392d418c562bba97a94b8
M8MBA <sauce.ix@gmail.com>
2022-06-25 13:05:24 -0600

    video categories landing

---
+++ client/src/components/Video/index.js
@@ -0,0 +1,137 @@
+import React from 'react';
+
+const Video = () => {
+
+  return (
+
+    <div class="grid-container">
+      <div class="grid-x grid-margin-x" data-equalizer="" data-equalize-on="medium" data-equalize-by-row="true" data-resize="20sa9g-eq" data-mutate="2ezo0l-eq" data-e="ul2cfj-e" data-events="mutate">
+
+        <div class="block subtitleanchorblock medium-12 cell">
+
+          <h2 id="VideoCollections">Video Collections </h2>
+
+          <!-- sub-title accent -->
+          <div class="grid-x grid-margin-x ">
+            <div class="small-3 medium-2 large-1 cell">
+              <div class="accent-general-background title-accent"></div>
+            </div>
+          </div></div><div class="block calloutblock medium-4 cell">
+
+
+
+          <div class="callout-block callout none Text ">
+            <div data-equalizer-watch="" style="height: 67px;">
+              <h4 class="heading">Muscle Training</h4>
+              <p style="padding-bottom: 1em;"></p>
+            </div>
+            <a class="button primary-ghost-button  expanded Text " title="View Collection" target="" href="">
+
+              <span>View Collection</span>
+            </a>
+          </div>
+
+        </div><div class="block calloutblock medium-4 cell">
+
+
+
+          <div class="callout-block callout none Text ">
+            <div data-equalizer-watch="" style="height: 67px;">
+              <h4 class="heading">Stretching</h4>
+              <p style="padding-bottom: 1em;"></p>
+            </div>
+            <a class="button primary-ghost-button  expanded Text " title="View Collection" target="" href="">
+
+              <span>View Collection</span>
+            </a>
+          </div>
+
+        </div><div class="block calloutblock medium-4 cell">
+
+          <div class="callout-block callout none Text ">
+            <div data-equalizer-watch="" style="height: 67px;">
+              <h4 class="heading">Placeholder</h4>
+              <p style="padding-bottom: 1em;"></p>
+            </div>
+            <a class="button primary-ghost-button  expanded Text " title="View Collection" target="" href="">
+
+              <span>View Collection</span>
+            </a>
+          </div>
+
+        </div><div class="block calloutblock medium-4 cell">
+
+
+
+          <div class="callout-block callout none Text ">
+            <div data-equalizer-watch="" style="height: 102px;">
+              <h4 class="heading">Placeholder</h4>
+              <p style="padding-bottom: 1em;"></p>
+            </div>
+            <a class="button primary-ghost-button  expanded Text " title="View Collection" target="" href="">
+
+              <span>View Collection</span>
+            </a>
+          </div>
+
+        </div><div class="block calloutblock medium-4 cell">
+
+
+
+          <div class="callout-block callout none Text ">
+            <div data-equalizer-watch="" style="height: 102px;">
+              <h4 class="heading">Placeholder</h4>
+              <p style="padding-bottom: 1em;"></p>
+            </div>
+            <a class="button primary-ghost-button  expanded Text " title="Bridge Videos" target="" href="/">
+
+              <span>Bridge Videos</span>
+            </a>
+          </div>
+
+        </div><div class="block calloutblock medium-4 cell">
+
+
+
+          <div class="callout-block callout none Text ">
+            <div data-equalizer-watch="" style="height: 102px;">
+              <h4 class="heading">Placeholder</h4>
+              <p style="padding-bottom: 1em;"></p>
+            </div>
+            <a class="button primary-ghost-button  expanded Text " title="View Collection" target="" href="/">
+              <span>View Collection</span>
+            </a>
+          </div>
+
+        </div><div class="block calloutblock medium-4 cell">
+
+
+
+          <div class="callout-block callout none Text ">
+            <div data-equalizer-watch="" style="height: 92px;">
+              <div class="label members-only solid none"><i class="fi-lock"></i> Members only</div>
+              <h4 class="heading">Placeholder</h4>
+              <p style="padding-bottom: 1em;"></p>
+            </div>
+            <a class="button primary-ghost-button  expanded Text " title="View Collection" target="" href="/">
+              <span>View Collection</span>
+            </a>
+          </div>
+
+        </div><div class="block calloutblock medium-4 cell">
+          <div class="callout-block callout none blackText ">
+            <div data-equalizer-watch="" style="height: 92px;">
+              <h4 class="heading">Placeholder</h4>
+              <p style="padding-bottom: 1em;"></p>
+            </div>
+            <a class="button primary-ghost-button  expanded blackText " title="View Collection"   target="" href="/">
+              <span>View Collection</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Video;
