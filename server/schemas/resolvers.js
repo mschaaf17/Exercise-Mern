3c1a5e29d034b8130f1305a8bb858363954488ee
Maddy <madisonschaaf48@gmail.com>
2022-06-26 16:57:06 -0600

    merging

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -137,10 +137,6 @@ const resolvers = {
 
       return { token, user };
     },
-<<<<<<< HEAD
-
-=======
->>>>>>> workout-fix
     addExerciseName: async (parent, args, context) => {
       if (context.user) {
         const exercise = await ExerciseCategory.create({
@@ -159,7 +155,6 @@ const resolvers = {
 
       throw new AuthenticationError('You need to be logged in!');
     },
-<<<<<<< HEAD
 
     editUser: async (parent, args, context) => {
       if (context.user)  {
@@ -171,8 +166,6 @@ const resolvers = {
       throw new AuthenticationError('You need to be logged in!');
     },
 
-=======
->>>>>>> workout-fix
     addExercise: async (parent, args, context) => {
       if (context.user) {
         console.log(context.user);
