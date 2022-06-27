a53c81c415093bea570ef7406c1b61f5529eaac0
jamesfill <jamesp.fillerup@gmail.com>
2022-06-26 20:02:39 -0600

    m

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -81,6 +81,7 @@ const resolvers = {
     
       throw new AuthenticationError('You need to be logged in!');
     },
+    
     editUser: async (parent, args, context) => {
       if (context.user)  {
         const user = await User.findOneAndUpdate({_id: context.user._id}, args, {new: true})
